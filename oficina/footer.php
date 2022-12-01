1b66d18cbdc49889e25c3b0f5ab4717d26cfb58d
Jariele <112036099+Jariele@users.noreply.github.com>
2022-11-30 21:32:32 -0300

    Add files via upload

--- oficina/footer.php
+++ oficina/footer.php
@@ -1,74 +1,72 @@
 <div class="container-fluid">
-        <div class="row text-center footer">
-            <div class="col-md-3">
-              <h3>conserta Tudo</h3>
-              <p>nossa oficina esta ha mais de 30 anos no mercado trabalhando serio por voce e sua seguranca.</p>
-            </div>
+    <div class="row text-center footer">
+      <div class="col-md-3">
+        <h3>Conserta Tudo</h3>
+        <p>Nossa oficina esta há mais de 30 anos no mercado trabalhando
+          sério por você e sua segurança.</p>
+      </div>
 
-            <div class="col-md-3">
-                <h3>contatos </h3>
-                  <p>(21)3334-7458</p>
-                    <p>(21)98776-3435</p>
-                      <p>(21)99775-1255</p>                     
-                 
-            </div>
+      <div class="col-md-3">
+        <h3>Contatos</h3>
+        <p>(21)3334-7458</p>
+        <p>(21)98776-3435</p>
+        <p>(21)99775-1255</p>
+      </div>
 
-            <div class="col-md-3">
-                <h2>horario de atendimento </h2>
-                <ul>
-                  <LI> segunda a sexta -08:00 as 17:00</LI>
-                  <LI>sabado -08:00 AS 13:00</LI>
+      <div class="col-md-3">
+        <h3>Horário de Atendimento</h3>
+        <ul>
+          <li>Segunda a Sexta - 08:00 às 17:00</li>
+          <li>Sábado - 08:00 às 13:00</li>
+        </ul>
+      </div>
 
-                </ul>
-            </div>
+      <div class="col-md-3">
+        <h3>Menu</h3>
+        <ul class="navbar-nav mr-auto menu2">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mecânica Geral</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Serviços Elétricos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nossa Oficina</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Fale Conosco</a>
+          </li>
+        </ul>
 
-            <div class="col-md-3">
-                <h3>menu 4</h2>
-               <ul class="navbar-nav mr-auto menu2">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
-                        </li>
-                        
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">mecanica geral</a>
-                        </li><li class="nav-item">
-                          <a class="nav-link" href="#">servicos eletricos</a>
-                        </li><li class="nav-item">
-                          <a class="nav-link" href="#">Nossa Oficina</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Fale Conosco</a>
-                        </li>
-                      </ul>
-            </div>
-            
-        </div>
+      </div>
 
-        <div class="row text-center sociais">
-
-          <div class="col-md-12">
-          
-          <a href="#"><img src="imagens/insta.jpg"></a>
-          <a href="#"><img src="imagens/face.png"></a>
-          <a href="#"><img src="imagens/watzap.jpg"></a>
-          
-          </div>
-        </div>
     </div>
 
+    <div class="row text-center sociais">
+      <div class="col-md-12">
+        <a href="#"><img src="imagens/whatsapp.png"></a>
+        <a href="#"><img src="imagens/face.png"></a>
+        <a href="#"><img src="imagens/insta.png"></a>
+      </div>
+    </div>
+  </div>
 
 
 
+  <!-- JavaScript (Opcional) -->
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</body>
 
-
-
-
-
-
-    <!-- JavaScript (Opcional) -->
-    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
 </html>
--- oficina/pecas.php
+++ oficina/pecas.php
@@ -2,62 +2,179 @@
 require_once 'head.php';
 require_once 'menu.php';
 ?>
+
 <div class="container-fluid">
-    <div class="row">
+    <div class="row pecas">
         <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Título do card</h5>
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>   
-        </div>   
-       
-        <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Título do card</h5>
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div> 
+            <div class="card" >
+                <img class="card-img-top" src="imagens/vela.jfif" alt="Imagem de capa do card">
+                <div class="card-body">
+                    <h5 class="card-title">vela</h5>
+                    <p class="card-text">A ngk e referencia no mercado de produtos automotivos.Vela de auto desempenho para veiculos flex.</p>
+                    <p>R$80,00</p>
+                   <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vela">
+comprar
+</button>
+                </div>
+            </div>
         </div>
+
         <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Título do card</h5>
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-        </div>   
-       
-        <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Título do card</h5>
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>  
+            <div class="card">
+                <img class="card-img-top" src="imagens/freio.jfif" alt="Imagem de capa do card">
+                <div class="card-body">
+                    <h5 class="card-title">freio</h5>
+                    <p class="card-text">alto desempenho e energia para seu carro.</p>
+                    <p>R$50,00</p>
+                    <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#freios">
+ comprar
+</button>
+                </div>
+            </div>
         </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img class="card-img-top" src="imagens/armotecedor.jfif" alt="Imagem de capa do card">
+                <div class="card-body">
+                    <h5 class="card-title">amortecedor</h5>
+                    <p class="card-text">alto desempenho e energia para seu carro. </p>
+                    <p>R$150,00</p>
+                   <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#amortecedor">
+ comprar
+</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img class="card-img-top" src="imagens/bieneta.jfif" alt="Imagem de capa do card">
+                <div class="card-body">
+                    <h5 class="card-title">bieleta</h5>
+                    <p class="card-text">bieleta -ande sempre com segurança e estabilidade.</p>
+                    <p>R$100,00</p>
+                    <!-- Botão para acionar modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#bieleta">
+ comprar
+</button>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </div>
 
 
+<!-- Modal -->
+<div class="modal fade" id="" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">vela</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicionar ao carinho</button>
+      </div>
+    </div>
+  </div>
+</div>
 
+<!-- Modal -->
+<div class="modal fade" id="amortecedor" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicionar ao carinho</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="vela" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <img class="img-fluid" src="imagens/vela.jfif">
+<h4>velas<h4>
+    <p>solucoes inteligentes para seu carro<p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicionar ao carinho</button>
+      </div>
+    </div>
+  </div>
+</div>
 
+<!-- Modal -->
+<div class="modal fade" id="bieleta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicionar ao carinho</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-
-
-
-
+<!-- Modal -->
+<div class="modal fade" id="freios" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">adicionar ao carinho</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
@@ -66,11 +183,3 @@ require_once 'menu.php';
 <?php
 require_once 'footer.php';
 ?>
-
-
-
-
-
-
-
-
